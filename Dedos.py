8043940e56d3f207dfa81fa467f5357ef9118fa0
Mutant-Aqua <idavidx3@gmail.com>
2021-07-17 20:06:22 -0500

    Primera parte culinada

--- Dedos.py
+++ Dedos.py
@@ -2,10 +2,9 @@ import cv2
 import mediapipe as mp
 import os
 
-
 #-------------------------------------Almacenamiento del entrenamiento----------------------------------
 nombre ='Letra_A'
-direccion ='C:/Users/Aqua_Mutant/Documents/Señas_py'
+direccion ='C:/Users/Aqua_Mutant/Documents/Proyectos_Python/Traductor'
 carpeta = direccion + '/' + nombre
 if not os.path.exists(carpeta):
     print('Carpeta creada: ',carpeta)
@@ -14,20 +13,21 @@ if not os.path.exists(carpeta):
 cont = 0
 
 #---------------Lectura de los archivos de la carpeta
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+#cap = cv2.VideoCapture(0)
 
 #-----------------------------Objeto que dara seguimiento a las manos
 clase_manos = mp.solutions.hands
 manos = clase_manos.Hands() #Se deja el primer perimetro en false para evitar que detecte en todo momento
                             #Solo detecta en confianza alta
                             #Segundo parametro: numero maximo de manos
-                            #TErcer parametro: confianza minima de deteccion
+                            #Tercer parametro: confianza minima de deteccion
                             #Cuarto parametro: confianza minima de seguimiento
 
 #----------------------------Metodo para dibujo de manos
 dibujo = mp.solutions.drawing_utils #Se dibujan los 21 puntos de las manos
 
-while(1):
+while (1):
     ret,frame = cap.read()
     color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     copia = frame.copy()
@@ -39,7 +39,7 @@ while(1):
         for mano in resultado.multi_hand_landmarks: #busca la mano en el listado del descriptor
             for id, lm in enumerate(mano.landmark):#Se obtiuene la iunformacion de la mano encontrada
                 #print(id,lm)#Se pasa a pixeles los numericos
-                alto, ancho, c = frame .shape # se toma la logintud de las manos
+                alto, ancho, c = frame.shape # se toma la logintud de las manos
                 corx, cory = int(lm.x*ancho), int(lm.y*alto)#Ubicacion de cada punto
                 posiciones.append([id,corx,cory])
                 dibujo.draw_landmarks(frame, mano, clase_manos.HAND_CONNECTIONS)
@@ -50,20 +50,18 @@ while(1):
                 pto_i4 = posiciones[0]
                 pto_i5 = posiciones[9]
                 x1, y1 = (pto_i5[1]-100),(pto_i5[2]-100)
-                ancho, alto = (x1+100),(y1+100)
+                ancho, alto = (x1+200),(y1+200)
                 x2,y2 = x1 + ancho, y1 + alto
                 dedos_reg = copia[y1:y2, x1:x2]
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
-        try:
-            dedos_reg = cv2.resize(dedos_reg, (50,50), interpolation = cv2.INTER_CUBIC)
-        except:
-            break
-            cv2.imwrite(carpeta + "/Dedos_{}.jpg".format(cont),dedos_reg)
+            dedos_reg = cv2.resize(dedos_reg, (200,200), interpolation=cv2.INTER_CUBIC)
+            cv2.imwrite(nombre+'/Dedos_{}.jpg'.format(cont),dedos_reg)
             cont = cont + 1
 
     cv2.imshow("Video",frame)
     k = cv2.waitKey(1)
-    if k ==27 or cont >= 100:
+    if k ==27 or cont >= 300:
         break
 cap.release()
 cv2.destroyAllWindows()
+print('Proceso de lectura "',nombre,'" compleatdo al 100%')
